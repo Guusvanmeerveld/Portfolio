aca2f3d35974523373ce5ddc1a0ec216ff5231b2
Guusvanmeerveld <guusvanmeerveld@live.nl>
2021-03-09 18:37:51 +0000

    Removed fieldset from form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -13,23 +13,21 @@ const Contact = () => (
         name="contact"
         data-netlify-recaptcha="true"
       >
-        <fieldset>
-          <label htmlFor="email">Email</label>
-          <input name="email" type="email" required placeholder="Your email address" id="email" />
-          <label htmlFor="type">Message type</label>
-          <select name="type[]" id="type">
-            <option value="bug">Bug</option>
-            <option value="question">Question</option>
-            <option value="suggestion">Suggestion</option>
-            <option value="other">Other</option>
-          </select>
-          <label htmlFor="message">Message</label>
-          <textarea required name="message" placeholder="Your message" id="message"></textarea>
-          <div data-netlify-recaptcha="true"></div>
-          <button className="button" type="submit">
-            Send
-          </button>
-        </fieldset>
+        <label htmlFor="email">Email</label>
+        <input name="email" type="email" required placeholder="Your email address" id="email" />
+        <label htmlFor="type">Message type</label>
+        <select name="type[]" id="type">
+          <option value="bug">Bug</option>
+          <option value="question">Question</option>
+          <option value="suggestion">Suggestion</option>
+          <option value="other">Other</option>
+        </select>
+        <label htmlFor="message">Message</label>
+        <textarea required name="message" placeholder="Your message" id="message"></textarea>
+        <div data-netlify-recaptcha="true"></div>
+        <button className="button" type="submit">
+          Send
+        </button>
       </form>
     </div>
   </div>
