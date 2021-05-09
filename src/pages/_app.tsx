c4083afe3ebfca72ea987cc5df29d2e82101f812
guusvanmeerveld <guusvanmeerveld@live.nl>
2021-05-09 21:14:15 +0100

    Moved to yarn, added milligram as npm package.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from 'next-themes';
 
-import "@styles/milligram.min.css";
-import "@styles/raleway.css";
+import 'milligram';
+import '@styles/raleway.css';
 
-import "@styles/sass/index.scss";
+import '@styles/sass/index.scss';
 
 function App({ Component, pageProps }) {
-  return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
 
 export default App;
