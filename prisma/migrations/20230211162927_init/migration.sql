4c383a2a35b108d73f24a19ef4d5433f5f5dda9c
Guus van Meerveld <mail@guusvanmeerveld.dev>
2023-02-13 02:36:39 +0100

    Do not ignore prisma migrations

---
+++ prisma/migrations/20230211162927_init/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "tags" TEXT[];
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "password" TEXT NOT NULL;
