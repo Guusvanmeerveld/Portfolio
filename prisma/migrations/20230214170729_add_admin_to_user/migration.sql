b24edaf8ebf7717ddbc97f6a1d55cbff31bd5f72
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-04-15 16:28:53 +0200

    remove prisma as its unused

--- prisma/migrations/20230214170729_add_admin_to_user/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "admin" BOOLEAN NOT NULL DEFAULT false,
-ALTER COLUMN "name" SET NOT NULL;
