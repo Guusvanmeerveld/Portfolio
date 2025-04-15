83c86c3beb2a663836315829c6ea76e166e776d9
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-04-15 18:34:27 +0200

    remove types from tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
 import { heroui } from "@heroui/react";
 
-import type { Config } from "tailwindcss" with { "resolution-mode": "import" };
+// import type { Config } from "tailwindcss" with { "resolution-mode": "import" };
 
-const config: Config = {
+const config = {
 	content: [
 		"./src/**/*.{js,ts,jsx,tsx,mdx}",
 		"./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
